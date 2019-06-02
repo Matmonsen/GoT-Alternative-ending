c76717a369aedfdc8e9894085a67abba30605124
Sindre Nerdal <sindre.nerdal@gmail.com>
2019-06-02 11:47:07 +0200

    Can navigate from start to game

---
+++ Assets/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public const int Scene_Menu = 0;
+    public const int Scene_Game = 1;
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
